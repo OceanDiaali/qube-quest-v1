dc6caffd5cff7155e7fad17d000619bea4f84922
OceanDiaali <ocean.diaali@gmail.com>
2019-03-13 19:02:00 +0100

    first commit

---
+++ Assets/Scripts/CountDownScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDownScript : MonoBehaviour {
+
+	public float timeLeft = 20.0f;
+	public Text startText;
+	
+	// Update is called once per frame
+	void Update () {
+		timeLeft -= Time.deltaTime;
+		startText.text = (timeLeft).ToString("0");
+		if (timeLeft <= 5) {
+			startText.color = Color.red;
+		}
+		if (timeLeft < 1)
+		{
+			//Do something useful or Load a new game scene depending on your use-case
+			startText.color = Color.white;
+			startText.text = "Time Up!";
+		}
+	}
+}
---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour {
+
+	//public Transform[] target;
+	public float speed;
+
+	//private int current;
+
+	
+	// Update is called once per frame
+	void Update () {
+//		if (transform.position != target [current].position) {
+//			Vector3 pos = Vector3.MoveTowards (transform.position, target [current].position, speed * Time.deltaTime);
+//			GetComponent<Rigidbody> ().MovePosition (pos);
+//		} else {
+//			current = (current + 1) % target.Length;
+//		}
+		//transform.position = new Vector3(Mathf.PingPong(Time.time, 2), transform.position.y, transform.position.z);
+		transform.position = new Vector3(Mathf.PingPong(Time.time, 1), transform.position.y, Mathf.PingPong(Time.time, 2));
+	}
+}
