07697d04abc27ab7e7db017aa27b10396edc66fb
OceanDiaali <ocean.diaali@gmail.com>
2019-03-14 18:38:59 +0100

    add code for token collection and player death

---
+++ Assets/Scripts/KillPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KillPlayer : MonoBehaviour {
+
+	//public GameObject particleExplosion;
+	//public GameObject blow;
+	public Text EndGame;
+
+	void Update() {
+	}
+
+
+	void OnCollisionEnter (Collision col) {
+			if (col.gameObject.name == "Player") {
+				Destroy (col.gameObject);
+			    EndGame.text = "You lost!";
+			    Time.timeScale = 0;
+				//Explode ();
+			}
+	}
+
+//	void Explode () {
+//		blow = Instantiate(particleExplosion, transform.position, Quaternion.identity);
+//		Destroy (blow, 1);
+//	}
+}
---
+++ Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreScript : MonoBehaviour {
+
+	void OnCollisionEnter(Collision col) {
+		if (col.gameObject.name == "Coin") {
+			Destroy (col.gameObject);
+		}
+
+		if (col.gameObject.name == "Coin1") {
+			Destroy (col.gameObject);
+		}
+
+		if (col.gameObject.name == "Coin2") {
+			Destroy (col.gameObject);
+		}
+	}
+}
